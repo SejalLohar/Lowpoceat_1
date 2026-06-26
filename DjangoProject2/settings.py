73bae8293bcc593c2de57f44d77ba04411c0717d
Sejal <156321301+SejalLohar@users.noreply.github.com>
2026-06-27 00:26:21 +0530

    Clear email credentials in settings.py
    Removed sensitive email credentials from settings.

--- DjangoProject2/settings.py
+++ DjangoProject2/settings.py
@@ -128,9 +128,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sejzzu.lohar@gmail.com'  # Replace with your Gmail email address
-EMAIL_HOST_PASSWORD = 'eeikqjbckokwarbc'  # Replace with your Gmail password or app-specific password
-DEFAULT_FROM_EMAIL = 'Your App <lowpoceat@gmail.com>'  # Replace with your desired "From" email address
+EMAIL_HOST_USER = ''  # Replace with your Gmail email address
+EMAIL_HOST_PASSWORD = ''  # Replace with your Gmail password or app-specific password
+DEFAULT_FROM_EMAIL = ''  # Replace with your desired "From" email address
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
